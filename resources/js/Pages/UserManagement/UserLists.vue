f80acf5d7a7c6051e1df0886c197ad9272bc5e20
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-08-11 16:02:05 +0800

    update

--- resources/js/Pages/UserManagement/UserLists.vue
+++ resources/js/Pages/UserManagement/UserLists.vue
@@ -50,20 +50,42 @@ const function_update = () => {
     <div class="flex flex-col mt-6">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <section class="text-gray-600 p-10">
-          <div class="container px-5  mx-auto">
-            <div class="flex flex-wrap w-full mb-5">
+          <div class="container px-5 mx-auto">
+            <!-- <div class="flex flex-wrap w-full mb-5">
               <div class="lg:w-1/2 w-full lg:mb-0">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Users</h1>
+                <h1
+                  class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"
+                >
+                  Users
+                </h1>
                 <div class="h-1 w-20 bg-yellow-500 rounded"></div>
               </div>
-            </div>
+            </div> -->
             <div class="flex flex-wrap -m-4">
-              <div v-for="(user, index) in props.users.data.filter(user => user.type != 0 && props.users.data.length != 0)" :key="index" class="xl:w-1/3  md:w-1/2 p-4">
-                <div class="bg-gray-50 border border-1 border-yellow-400 shadow-xl p-6 rounded-lg">
-                  <img class="h-40 rounded w-full object-cover object-center mb-6" :src="user.profile_photo_url" :alt="user.name"
-                    alt="content">
-                  <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{ user.email }}</h3>
-                  <h2 class="text-lg text-gray-900 font-medium title-font mb-4"> {{ user.name }}</h2>
+              <div
+                v-for="(user, index) in props.users.data.filter(
+                  (user) => user.type != 0 && props.users.data.length != 0
+                )"
+                :key="index"
+                class="xl:w-1/3 md:w-1/2 p-4"
+              >
+                <div
+                  class="bg-gray-50 border border-1 border-yellow-400 shadow-xl p-6 rounded-lg"
+                >
+                  <img
+                    class="h-40 rounded max-w- object-fit object-center mb-6 mx-auto"
+                    :src="user.profile_photo_url"
+                    :alt="user.name"
+                    alt="content"
+                  />
+                  <h3
+                    class="tracking-widest text-indigo-500 text-xs font-medium title-font"
+                  >
+                    {{ user.email }}
+                  </h3>
+                  <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
+                    {{ user.name }}
+                  </h2>
                   <div class="flex mt-4 gap-1">
                     <Icon icon="sex" size="sm" />
                     <p class="text-sm font-medium">
@@ -88,7 +110,12 @@ const function_update = () => {
                       {{ user.address }}
                     </p>
                   </div>
-                  <p v-if="user.type != 0" class="leading-relaxed font-bold mt-5 mb-2 text-base">Controls</p>
+                  <p
+                    v-if="user.type != 0"
+                    class="leading-relaxed font-bold mt-5 mb-2 text-base"
+                  >
+                    Controls
+                  </p>
                   <div v-if="user.type != 0" class="text-sm whitespace-nowrap">
                     <Dropdown :status="user.type" :id="user.id" />
                   </div>
@@ -100,25 +127,33 @@ const function_update = () => {
                       </p>
                     </div>
                   </div>
-                  <div v-if="user.type != 0"
-                    class="text-sm mt-2 flex justify-center w-100  whitespace-nowrap">
-                    <button @click="function_open_modal_confirmation(true, user)"
-                      class="p-2  bg-green-400 rounded-lg hover:bg-green-600 " v-if="user.status == 0 && user.type != 0">
+                  <div
+                    v-if="user.type != 0"
+                    class="text-sm mt-2 flex justify-center w-100 whitespace-nowrap"
+                  >
+                    <button
+                      @click="function_open_modal_confirmation(true, user)"
+                      class="p-2 bg-green-400 rounded-lg hover:bg-green-600"
+                      v-if="user.status == 0 && user.type != 0"
+                    >
                       Activate
                     </button>
-                    <button @click="function_open_modal_confirmation(false, user)"
-                      class="p-2  bg-red-400 rounded-lg hover:bg-red-600" v-else-if="user.status == 1 && user.type != 0">
+                    <button
+                      @click="function_open_modal_confirmation(false, user)"
+                      class="p-2 bg-red-400 rounded-lg hover:bg-red-600"
+                      v-else-if="user.status == 1 && user.type != 0"
+                    >
                       Deactivate
                     </button>
                   </div>
-
                 </div>
               </div>
-              <div v-if="props.users.data.length == 0" class="mx-auto text-center">
-              <p class="text-4xl text-gray-700">No users!</p>
-            </div>
-
-
+              <div
+                v-if="props.users.data.length == 0"
+                class="mx-auto text-center"
+              >
+                <p class="text-4xl text-gray-700">No users!</p>
+              </div>
             </div>
           </div>
         </section>
@@ -132,7 +167,11 @@ const function_update = () => {
       </p>
     </div>
   </section>
-  <ConfirmDialogModal :show="condfirmationModal" @close="condfirmationModal = false" maxWidth="2xl">
+  <ConfirmDialogModal
+    :show="condfirmationModal"
+    @close="condfirmationModal = false"
+    maxWidth="2xl"
+  >
     <template #title> Are you sure you want to update this user?</template>
     <template #content>
       <p class="text-red-500">
@@ -143,13 +182,26 @@ const function_update = () => {
       <SecondaryButton @click="condfirmationModal = false" class="mr-2">
         nevermind
       </SecondaryButton>
-      <Button :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
-        class="bg-green-200 hover:bg-green-400" @click="function_update">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-          stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-        </svg>&nbsp;Submit
+      <Button
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing"
+        class="bg-green-200 hover:bg-green-400"
+        @click="function_update"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-auto"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+          /></svg
+        >&nbsp;Submit
       </Button>
     </template>
   </ConfirmDialogModal>
