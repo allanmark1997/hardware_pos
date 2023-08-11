a650a616d8e3964f72255cddb13791214e77a800
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-08-11 15:04:27 +0800

    update

--- app/Models/product.php
+++ app/Models/product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -25,5 +26,11 @@ class Product extends Model
     }
     public function current_discount(){
         return $this->hasOne(sale_discount::class, "product_id", "id")->latestOfMany();
+    }
+    public static function initStorage()
+    {
+        if (!Storage::exists('public/images/products')) {
+            Storage::makeDirectory('public/images/products');
+        }
     }
 }
