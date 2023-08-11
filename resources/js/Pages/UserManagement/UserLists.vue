bad0e1a97e455230cd9dfe8869bd3d6a312970c0
MrRegei <39606446+MrRegei@users.noreply.github.com>
2023-08-11 15:49:06 +0800

    Update
    Update

--- resources/js/Pages/UserManagement/UserLists.vue
+++ resources/js/Pages/UserManagement/UserLists.vue
@@ -49,195 +49,79 @@ const function_update = () => {
   <section class="container px-4 mx-auto">
     <div class="flex flex-col mt-6">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-          <div class="overflow-hidden border border-gray-200">
-            <table class="min-w-full divide-y divide-gray-200">
-              <thead class="bg-yellow-400">
-                <tr>
-                  <th
-                    scope="col"
-                    class="py-3.5 px-4 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    <button
-                      class="flex items-center gap-x-3 focus:outline-none"
-                    >
-                      <span>User</span>
-
-                      <svg
-                        class="h-3"
-                        viewBox="0 0 10 11"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                          fill="currentColor"
-                          stroke="currentColor"
-                          stroke-width="0.1"
-                        />
-                        <path
-                          d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                          fill="currentColor"
-                          stroke="currentColor"
-                          stroke-width="0.1"
-                        />
-                        <path
-                          d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                          fill="currentColor"
-                          stroke="currentColor"
-                          stroke-width="0.3"
-                        />
-                      </svg>
-                    </button>
-                  </th>
-
-                  <th
-                    scope="col"
-                    class="px-12 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    Sex
-                  </th>
-
-                  <th
-                    scope="col"
-                    class="px-4 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    Birthday & Contact #
-                  </th>
-
-                  <th
-                    scope="col"
-                    class="px-4 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    Hometown
-                  </th>
-
-                  <!-- <th
-                    scope="col"
-                    class="px-4 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    Contact #
-                  </th> -->
-
-                  <th
-                    scope="col"
-                    class="px-4 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    User Type
-                  </th>
-
-                  <th
-                    scope="col"
-                    class="px-4 py-3.5 text-sm font-bold text-left rtl:text-right text-gray-900"
-                  >
-                    Actions
-                  </th>
-                </tr>
-              </thead>
-
-              <tbody class="bg-white divide-y divide-gray-200">
-                <tr v-for="(user, index) in props.users.data" :key="index">
-                  <td class="px-4 py-4 font-medium whitespace-nowrap">
-                    <div class="flex gap-1">
-                      <div class="my-auto">
-                        <img
-                          class="w-10 h-10 rounded-full"
-                          :src="user.profile_photo_url"
-                          :alt="user.name"
-                        />
-                      </div>
-                      <div>
-                        <h2 class="text-sm font-medium text-gray-800">
-                          {{ user.name }}
-                        </h2>
-                        <p class="text-sm font-normal text-gray-600">
-                          {{ user.email }}
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td
-                    class="px-12 py-4 whitespace-nowrap"
-                    :class="user.sex == 0 ? 'text-pink-600' : 'text-blue-600'"
-                  >
-                    <div class="flex gap-1">
-                      <Icon icon="sex" size="sm" />
-                      <p class="text-sm font-medium">
-                        {{ user.sex == 0 ? "Female" : "Male" }}
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-4 text-sm whitespace-nowrap">
-                    <div class="flex gap-1 mb-1">
-                      <Icon icon="birthday" size="sm" />
-                      <h4 class="text-gray-700">
-                        {{ function_extract_date_time(user.bday) }}
-                      </h4>
-                    </div>
-                    <div class="flex gap-2">
-                      <Icon icon="mobile" size="sm" />
-                      <p class="text-sm">
-                        {{ user.contact_no }}
-                      </p>
-                    </div>
-                  </td>
-
-                  <td class="px-4 py-4 whitespace-nowrap">
-                    <div class="flex gap-2">
-                      <Icon icon="location" size="sm" />
-                      <p class="text-sm">
-                        {{ user.address }}
-                      </p>
-                    </div>
-                  </td>
-                  <!-- <td class="px-4 py-4 whitespace-nowrap">
-                    <div class="flex gap-2">
-                      <Icon icon="mobile" size="sm" />
-                      <p class="text-sm">
-                        {{ user.contact_no }}
-                      </p>
-                    </div>
-                  </td> -->
-
-                  <td v-if="user.type != 0" class="text-sm whitespace-nowrap">
+        <section class="text-gray-600 p-10">
+          <div class="container px-5  mx-auto">
+            <div class="flex flex-wrap w-full mb-5">
+              <div class="lg:w-1/2 w-full lg:mb-0">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Users</h1>
+                <div class="h-1 w-20 bg-yellow-500 rounded"></div>
+              </div>
+            </div>
+            <div class="flex flex-wrap -m-4">
+              <div v-for="(user, index) in props.users.data.filter(user => user.type != 0 && props.users.data.length != 0)" :key="index" class="xl:w-1/3  md:w-1/2 p-4">
+                <div class="bg-gray-50 border border-1 border-yellow-400 shadow-xl p-6 rounded-lg">
+                  <img class="h-40 rounded w-full object-cover object-center mb-6" :src="user.profile_photo_url" :alt="user.name"
+                    alt="content">
+                  <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{{ user.email }}</h3>
+                  <h2 class="text-lg text-gray-900 font-medium title-font mb-4"> {{ user.name }}</h2>
+                  <div class="flex mt-4 gap-1">
+                    <Icon icon="sex" size="sm" />
+                    <p class="text-sm font-medium">
+                      {{ user.sex == 0 ? "Female" : "Male" }}
+                    </p>
+                  </div>
+                  <div class="flex mt-4 gap-1 mb-1">
+                    <Icon icon="birthday" size="sm" />
+                    <h4 class="text-gray-700">
+                      {{ function_extract_date_time(user.bday) }}
+                    </h4>
+                  </div>
+                  <div class="flex mt-4 gap-2">
+                    <Icon icon="mobile" size="sm" />
+                    <p class="text-sm">
+                      {{ user.contact_no }}
+                    </p>
+                  </div>
+                  <div class="flex mt-4 gap-2">
+                    <Icon icon="location" size="sm" />
+                    <p class="text-sm">
+                      {{ user.address }}
+                    </p>
+                  </div>
+                  <p v-if="user.type != 0" class="leading-relaxed font-bold mt-5 mb-2 text-base">Controls</p>
+                  <div v-if="user.type != 0" class="text-sm whitespace-nowrap">
                     <Dropdown :status="user.type" :id="user.id" />
-                  </td>
-                  <td v-else class="text-sm whitespace-nowrap">
+                  </div>
+                  <div v-else class="text-sm mt-3 whitespace-nowrap">
                     <div class="flex my-auto gap-1">
                       <Icon icon="administrator" size="sm" />
-
                       <p class="text-sm text-gray-700 text-center font-bold">
                         Root admin
                       </p>
                     </div>
-                  </td>
-                  <td v-if="user.type != 0" class="text-sm whitespace-nowrap">
-                    <button
-                      @click="function_open_modal_confirmation(true, user)"
-                      class="p-2 bg-green-400 rounded-lg hover:bg-green-600 mr-2"
-                      v-if="user.status == 0 && user.type != 0"
-                    >
+                  </div>
+                  <div v-if="user.type != 0"
+                    class="text-sm mt-2 flex justify-center w-100  whitespace-nowrap">
+                    <button @click="function_open_modal_confirmation(true, user)"
+                      class="p-2  bg-green-400 rounded-lg hover:bg-green-600 " v-if="user.status == 0 && user.type != 0">
                       Activate
                     </button>
-                    <button
-                      @click="function_open_modal_confirmation(false, user)"
-                      class="p-2 bg-red-400 rounded-lg hover:bg-red-600"
-                      v-else-if="user.status == 1 && user.type != 0"
-                    >
+                    <button @click="function_open_modal_confirmation(false, user)"
+                      class="p-2  bg-red-400 rounded-lg hover:bg-red-600" v-else-if="user.status == 1 && user.type != 0">
                       Deactivate
                     </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <div
-              v-if="props.users.data.length == 0"
-              class="mx-auto text-center"
-            >
+                  </div>
+
+                </div>
+              </div>
+              <div v-if="props.users.data.length == 0" class="mx-auto text-center">
               <p class="text-4xl text-gray-700">No users!</p>
             </div>
+
+
+            </div>
           </div>
-        </div>
+        </section>
       </div>
     </div>
 
@@ -248,11 +132,7 @@ const function_update = () => {
       </p>
     </div>
   </section>
-  <ConfirmDialogModal
-    :show="condfirmationModal"
-    @close="condfirmationModal = false"
-    maxWidth="2xl"
-  >
+  <ConfirmDialogModal :show="condfirmationModal" @close="condfirmationModal = false" maxWidth="2xl">
     <template #title> Are you sure you want to update this user?</template>
     <template #content>
       <p class="text-red-500">
@@ -263,26 +143,13 @@ const function_update = () => {
       <SecondaryButton @click="condfirmationModal = false" class="mr-2">
         nevermind
       </SecondaryButton>
-      <Button
-        :class="{ 'opacity-25': form.processing }"
-        :disabled="form.processing"
-        class="bg-green-200 hover:bg-green-400"
-        @click="function_update"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-auto"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
-          /></svg
-        >&nbsp;Submit
+      <Button :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+        class="bg-green-200 hover:bg-green-400" @click="function_update">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+          stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+        </svg>&nbsp;Submit
       </Button>
     </template>
   </ConfirmDialogModal>
