2ea63061f1eda214d636cc49f85cecbe73df634b
MrRegei <39606446+MrRegei@users.noreply.github.com>
2023-08-14 23:36:05 +0800

    sdsd
    ssd

--- resources/js/Pages/Cashier/Cashier.vue
+++ resources/js/Pages/Cashier/Cashier.vue
@@ -5,7 +5,7 @@ import { ref } from "vue";
 
 
 const totalCart = ref(2)
-const sampleData = ref(0)
+const sampleData = ref(5)
 const samplePurchaseData = ref(2)
 
 const firstDigit = (num) => {
@@ -42,7 +42,7 @@ const props = defineProps(["props_detail"]);
     <div class="py-5">
       <div class="max-w-7xl mx-auto bg-white rounded mt-5 px-1">
         <div class="grid grid-cols-12 gap-2">
-          <div class="col-span-8 p-5">
+          <div class="col-span-7 p-5">
 
             <form class="flex items-center">
               <label for="Search products" class="sr-only">Search</label>
@@ -104,7 +104,7 @@ const props = defineProps(["props_detail"]);
                 </table>
 
               </div>
-              <div class="mt-24 flex justify-center">
+              <div class="mt-24 flex justify-center" v-if="sampleData == 0">
                 <div class="bg-white px-10 py-5 shadow-lg rounded xl">
                   <div class="flex justify-center mb-2">
                     <Icon icon="shopping_bag" size="md" />
@@ -114,8 +114,9 @@ const props = defineProps(["props_detail"]);
               </div>
             </div>
           </div>
-          <div class="col-span-4  bg-gray-50">
-            <div class="bg-white rounded-b-xl shadow-md p-5 flex justify-between">
+          <div class="col-span-5   ">
+            <div class="bg-white rounded-b-xl shadow-md p-5 flex justify-between max-h-[63vmin] ">
+
               <div class="flex">
                 <Icon icon="shopping_cart" size="sm"></Icon>
                 <span class="font-bold">Cart</span>
@@ -131,7 +132,22 @@ const props = defineProps(["props_detail"]);
                 </span>
               </div>
             </div>
-            <div class="relative mt-5 px-2 overflow-x-auto  shadow-md">
+            <form class="flex mt-5 mx-5 items-center">
+              <label for="Search products" class="sr-only">Search</label>
+              <div class="relative w-full">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <Icon class="mr-1" icon="shopping_bag" size="xs" />
+                </div>
+                <input type="text" id="Search products"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full pl-10 p-2.5  "
+                  placeholder="Search products">
+              </div>
+              <button type="submit"
+                class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-yellow-700 rounded-lg border border-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 ">
+                <Icon class="mr-5" icon="search_icon" size="xs" />
+              </button>
+            </form>
+            <div class="relative mt-5 px-2 overflow-x-auto min-h-[45vmin] shadow-md">
               <table class="w-full text-sm text-left text-gray-500">
                 <tbody>
                   <tr v-for="data in totalCart" class="bg-white border-b ">
@@ -141,23 +157,52 @@ const props = defineProps(["props_detail"]);
                     <td class="px-6 py-4">
                       <div class="flex items-center space-x-3">
                         <div>
-                          <input type="number" id="first_product"
-                            class="bg-gray-50 w-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block px-2.5 py-1 "
-                            placeholder="1" disabled>
+                          <div class="flex items-center space-x-3">
+                            <button
+                              class="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 "
+                              type="button">
+                              <span class="sr-only">Quantity button</span>
+                              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 18 2">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                  stroke-width="2" d="M1 1h16" />
+                              </svg>
+                            </button>
+                            <div>
+                              <input type="number" id="first_product"
+                                class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="1" required>
+                            </div>
+                            <button
+                              class="inline-flex items-center justify-center h-6 w-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 "
+                              type="button">
+                              <span class="sr-only">Quantity button</span>
+                              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                  stroke-width="2" d="M9 1v16M1 9h16" />
+                              </svg>
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </td>
                     <td class="px-6 py-4 font-semibold text-gray-900 ">
-                      $999
+                      $9992
                     </td>
                     <td class="px-6 py-4 font-semibold text-gray-900 ">
                       <button>
-                      <Icon icon="close_icon" size="sm" class="text-red" />
-                    </button>
+                        <Icon icon="close_icon" size="sm" class="text-red" />
+                      </button>
                     </td>
                   </tr>
                 </tbody>
               </table>
+            </div>
+            <div class="bg-white flex justify-between item-center p-5">
+              <p><span class="font-bold">Total:</span> 200</p>
+              <button type="button" class="focus:outline-none text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Charge</button>
+             
             </div>
           </div>
         </div>
