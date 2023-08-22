6afeff22d7a826f77978b778f76dcf92998a3b58
MrRegei <39606446+MrRegei@users.noreply.github.com>
2023-08-22 22:30:55 +0800

    sdds
    sdsd

---
+++ resources/js/Components/buttonGroup.vue
@@ -0,0 +1,53 @@
+<script setup>
+import Modal from './Modal.vue';
+
+const emit = defineEmits(['close']);
+
+defineProps({
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    maxWidth: {
+        type: String,
+        default: '2xl',
+    },
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const close = () => {
+    emit('close');
+};
+</script>
+
+<template>
+    <Modal
+        :show="show"
+        :max-width="maxWidth"
+        :closeable="closeable"
+        @close="close"
+    >
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+        
+
+                <div class="mt-3 w-full text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 class="text-lg font-medium text-gray-900">
+                        <slot name="title" />
+                    </h3>
+
+                    <div class="mt-4 flex justify-center  text-sm text-gray-600">
+                        <slot class="mx-auto" name="content" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-right">
+            <slot name="footer" />
+        </div>
+    </Modal>
+</template>
