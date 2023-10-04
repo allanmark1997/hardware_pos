53f01df700240b7e4eb51c4c2fed47a6effbe1d0
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-10-04 23:27:45 +0800

    update

---
+++ resources/js/helpers/helper.js
@@ -0,0 +1,27 @@
+<script>
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+document.onkeydown = (e) => {
+  if (e.key == "F12" || e.key == 123) {
+    e.preventDefault();
+  }
+  //   if (e.key == "F5" || e.key == 116) {
+  //     e.preventDefault();
+  //   }
+  if (e.ctrlKey && e.shiftKey && e.key == "I") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "C") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "J") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == "U") {
+    e.preventDefault();
+  }
+};
+</script>
