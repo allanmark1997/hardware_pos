4ae1f91c172f9e3194e48b7994319499ef37d48d
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-10-09 22:13:21 +0800

    update

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,88 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Welcome from "@/Components/Welcome.vue";
+const year_sales = [
+  {
+    name: "Niknaks",
+    data: {
+      January: 1,
+      February: 2,
+      April: 8,
+      May: 2,
+      June: 4,
+      July: 11,
+      August: 12,
+      September: 160,
+      October: 128,
+      November: 110,
+      December: 10,
+    },
+  },
+  {
+    name: "Call parents",
+    data: {
+      January: 2,
+      February: 4,
+      April: 10,
+      May: 20,
+      June: 40,
+      July: 110,
+      August: 120,
+      September: 160,
+      October: 128,
+      November: 110,
+      December: 10,
+    },
+  },
+];
+
+const month_sales = [
+  {
+    name: "Niknaks",
+    data: {
+      January: 1,
+      February: 2,
+      April: 8,
+      May: 2,
+      June: 4,
+      July: 11,
+      August: 12,
+      September: 160,
+      October: 128,
+      November: 110,
+      December: 10,
+    },
+  },
+  {
+    name: "Call parents",
+    data: {
+      January: 2,
+      February: 4,
+      April: 10,
+      May: 20,
+      June: 40,
+      July: 110,
+      August: 120,
+      September: 160,
+      October: 128,
+      November: 110,
+      December: 10,
+    },
+  },
+];
+
+const top_ten = [
+  { name: "sample", sale: "sample" },
+  { name: "sample2", sale: "sample1" },
+  { name: "sample3", sale: "sample2" },
+  { name: "sample4", sale: "sample3" },
+  { name: "sample5", sale: "sample4" },
+  { name: "sample6", sale: "sample5" },
+  { name: "sample7", sale: "sample6" },
+  { name: "sample8", sale: "sample7" },
+  { name: "sample9", sale: "sample8" },
+  { name: "sample10", sale: "sample9" },
+];
 </script>
 
 <template>
@@ -14,7 +96,39 @@ import Welcome from "@/Components/Welcome.vue";
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          <Welcome />
+          <!-- <Welcome /> -->
+          <p class="p-2 text-xl font-bold">Year sales</p>
+          <line-chart :data="year_sales" />
+        </div>
+        <div
+          class="bg-transparent overflow-hidden shadow-xl sm:rounded-lg mt-4 mb-2"
+        >
+          <div class="grid grid-cols-12 gap-2">
+            <div class="col-span-4 border bg-white rounded-lg">
+              <p class="p-2 text-xl font-bold">This Month sales</p>
+              <column-chart :data="year_sales" />
+            </div>
+            <div class="col-span-4 border bg-white rounded-lg">
+              <p class="p-2 text-xl font-bold">This Week sales</p>
+              <area-chart :data="year_sales" />
+            </div>
+            <div class="col-span-4 border bg-white rounded-lg">
+              <p class="p-2 text-xl font-bold">Top 10 Products</p>
+              <div class="grid grid-cols-12 gap-2 p-4">
+                <template v-for="(top, key) in top_ten" :key="key">
+                  <div class="col-span-6">
+                    <p>{{ top.name }}</p>
+                  </div>
+                  <div class="col-span-6 text-right">
+                    <p>{{ top.sale }}</p>
+                  </div>
+                </template>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <p class="p-2 text-xl font-bold">Transactions</p>
         </div>
       </div>
     </div>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -15,6 +17,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(VueChartkick)
             .mount(el);
     },
     progress: {
