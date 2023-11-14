6bb89901b18fb9ccb093b32cf5159b69021fd5fb
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-11-14 21:07:38 +0800

    added build  security

--- public/build/assets/BarcodeReturn-c62a7304.js
+++
@@ -1,1 +0,0 @@
-import{c,e as r,o as t}from"./app-94d51bee.js";import a from"./BarcodeSVG2-17e2f1f5.js";const d={__name:"BarcodeReturn",props:["code"],setup(o){const e=o;return(n,s)=>e.code!=""?(t(),c(a,{key:0,code:e.code},null,8,["code"])):r("",!0)}};export{d as default};
