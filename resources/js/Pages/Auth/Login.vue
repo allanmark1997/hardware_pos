2c026877fa879ffb82492a2d3987dbbe2fa777ac
Allan Mark G. Cabug-os <allanmark1997@gmail.com>
2024-02-16 02:35:45 +0800

    update

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -175,7 +175,7 @@ const update_expiry = () => {
           <div class="p-2 w-full">
             <div class="relative">
               <label for="name" class="leading-7 text-sm text-gray-600">Secret code</label>
-              <input v-model="expire_data.code" type="password" id="name" name="name" placeholder="Enter email" required
+              <input v-model="expire_data.code" type="password" id="name" name="name" placeholder="Enter secret code" required
                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8" />
               <span class="text-red-500">{{
                 usePage().props.errors.expire
